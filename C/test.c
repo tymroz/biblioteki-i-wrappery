7092e794052c4cd03c221faeb3cafb08b64a1847
tymoteusz <tymoteusz.rozmiarek89@gmail.com>
2024-03-28 15:48:56 +0100

    poprawa bledu w tescie

--- C/test.c
+++ C/test.c
@@ -4,8 +4,8 @@ int main(){
     printf("rekurencja: 7! = %llu\n", silnia_rekurencja(7));
     printf("petla: 7! = %llu\n", silnia_petla(7));
 
-    printf("rekurencja: NWD(170, 136) = %d\n", NWD_rekurencja(170, 136));
-    printf("petla: NWD(170, 136) = %d\n", NWD_petla(170, 136));
+    printf("rekurencja: NWD(170, 136) = %u\n", NWD_rekurencja(170, 136));
+    printf("petla: NWD(170, 136) = %u\n", NWD_petla(170, 136));
 
     return 0;
 }
