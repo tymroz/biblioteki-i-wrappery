d4cf411ed6d2ae3f119c34b70b646131eb77e116
tymroz <135758015+tymroz@users.noreply.github.com>
2024-03-28 15:06:53 +0100

    implementacja silni i nwd

---
+++ C/biblioteka.h
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+unsigned long long silnia_rekurencja(int n){
+	if (n == 0 || n == 1){
+		return 0;
+	} else {
+		return n*silnia_rekurencja(n-1);
+	}
+}
+
+unsigned long long silnia_petla(int n){
+	unsigned long long wynik = 1;
+	for (int i = 1; i <= n; i++){
+		wynik = wynik*i;
+	}
+	return wynik;
+}
+
+unsigned int NWD_rekurencja(int a, int b){
+	if(b == 0){
+		return a;
+	} else {
+		return NWD_rekurencja(b, a % b);
+	}
+}
+
+unsigned int NWD_petla(int a, int b){
+	while(b != 0){
+		int temp = b;
+		b = a % b;
+		a = temp;
+	}
+	return a;
+}
