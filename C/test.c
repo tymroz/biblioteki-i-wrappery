758de22ff35b4db1efbabec644224c04cd8c7291
tymoteusz <tymoteusz.rozmiarek89@gmail.com>
2024-03-29 17:09:15 +0100

    drobne poprawki

--- C/test.c
+++ C/test.c
@@ -19,8 +19,8 @@ int main(){
     if(c % wynik_rek.nwd != 0){
         printf("Brak rozwiazan\n\n");
     } else{
-        printf("rekurencyjne: x = %d + %d * k, y = %d - %d * k\n", wynik_rek.x, wynik_rek.b/wynik_rek.nwd, wynik_rek.y, wynik_rek.a/wynik_rek.nwd);
-        printf("petla: x = %d + %d * k, y = %d - %d * k\n\n", wynik_petla.x, wynik_petla.b / wynik_petla.nwd, wynik_petla.y, wynik_petla.a / wynik_petla.nwd);
+        printf("rekurencyjne: x = %d + %dk, y = %d - %dk\n", wynik_rek.x, wynik_rek.b/wynik_rek.nwd, wynik_rek.y, wynik_rek.a/wynik_rek.nwd);
+        printf("petla: x = %d + %dk, y = %d - %dk\n\n", wynik_petla.x, wynik_petla.b / wynik_petla.nwd, wynik_petla.y, wynik_petla.a / wynik_petla.nwd);
     }
     
     return 0;
